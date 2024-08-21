8ed84646b573f41c2e2cb79e6b570b04ca59c2b0
Steve <ppsel03@gmail.com>
2024-08-21 09:36:17 -0500

    LOS5.5.py Batch install and run!

--- LOSinstallAndRUN.bat
+++ LOSinstallAndRUN.bat
@@ -1,5 +1,20 @@
 @echo off
+@echo off
 
+REM Check if LOS5.5.py exists
+if exist LOS5.5.py (
+    echo LOS5.5.py already exists.
+) else (
+    echo Downloading LOS5.5.py...
+    powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/StevoKeano/mestastic-utils/main/LOS5.5.py' -OutFile 'LOS5.5.py'"
+    if errorlevel 1 (
+        echo Failed to download LOS5.5.py
+        exit /b 1
+    ) else (
+        echo LOS5.5.py downloaded successfully.
+    )
+)
+rename LOS5.5.py los5.5.py
 REM Check if myenv directory exists
 if exist myenv (
     echo Virtual environment 'myenv' already exists. Activating...
@@ -32,6 +47,7 @@ REM Install specific version of SRTM.py
 pip install https://files.pythonhosted.org/packages/e6/07/1dc35011d5e68b6c873632fe07a517a1ad484bf4757ae4736320c4cab8ef/SRTM.py-0.3.7.tar.gz
 
 echo All packages installed.
+:app
 @echo off
 echo.
 echo  _      ____   _____       ___    ____  ____  
@@ -46,4 +62,4 @@ REM Run your Python script
 python los5.5.py
 
 REM Keep the window open
-pause
+goto :app
